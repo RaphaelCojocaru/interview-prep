b4a1c3a45d239bbcc461147fa65eb5d2a7c4d306
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-07-05 13:59:35 +0300

    tough question

--- HackerRank/Implementation/bigger-is-greater/Solution.java
+++ HackerRank/Implementation/bigger-is-greater/Solution.java
@@ -4,9 +4,78 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+// https://www.hackerrank.com/challenges/bigger-is-greater
 public class Solution {
 
+    public static String nextPermutation(String s) {
+		// Find non-increasing suffix
+        char[] array = s.toCharArray();
+		int i = array.length - 1;
+		while (i > 0 && array[i - 1] >= array[i])
+			i--;
+		if (i <= 0)
+			return "no answer";
+		
+		// Find successor to pivot
+		int j = array.length - 1;
+		while (array[j] <= array[i - 1])
+			j--;
+		char temp = array[i - 1];
+		array[i - 1] = array[j];
+		array[j] = temp;
+		
+		// Reverse suffix
+		j = array.length - 1;
+		while (i < j) {
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+			j--;
+		}
+		return String.valueOf(array);
+	}
+    
     public static String solveIt(String s) {
+        char[] arr = s.toCharArray();
+        char aux;
+        int start = 0, end = arr.length - 1;
+        int j = 0, i = 0;
+        
+        // find the first switching situation
+        for (i = arr.length - 1; i > 0; i--) {
+            j = i - 1;
+            if (arr[i] > arr[j])
+                break;
+        }
+        
+        if (i == 0)
+            return "no answer";
+        
+        // find the minimum/first character bigger than the pivot
+        for (i = end; i > j; i--)
+            if (arr[i] > arr[j]) {
+                aux = arr[i];
+                arr[i] = arr[j];
+                arr[j] = aux;             
+                break;
+            }
+        
+        // reorder/reverse the suffix of the string
+		i = arr.length - 1;
+        j++;
+		while (j < i) {
+			aux = arr[i];
+			arr[i] = arr[j];
+			arr[j] = aux;
+			i--;
+			j++;
+		}   
+     
+        return String.valueOf(arr);            
+    }
+    
+    public static String solveIt_orig(String s) {
         char[] arr = s.toCharArray();
         int start = 0, end = arr.length - 1;
         boolean change = false;
@@ -32,8 +101,6 @@ public class Solution {
         if (arr.length - j < 3)
             return String.valueOf(arr);
         
-  
-        
         for (int i = j + 1; i < arr.length - 1; i++)
             for (int k = i + 1; k < arr.length; k++)
                 if (arr[i] > arr[k]) {
@@ -52,6 +119,7 @@ public class Solution {
         for (int i = 0; i < noWords; i++) {
             String s = sc.next();
             System.out.println(solveIt(s));
+           // System.out.println(nextPermutation(s));
         }
             
     }
