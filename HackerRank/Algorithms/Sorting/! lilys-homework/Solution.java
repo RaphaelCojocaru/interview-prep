9a922d1f4e1009e9c8be4b342598c2e9f463686b
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-07-25 23:07:47 +0300

    finish this tough question

--- HackerRank/Algorithms/Sorting/! lilys-homework/Solution.java
+++ HackerRank/Algorithms/Sorting/! lilys-homework/Solution.java
@@ -51,40 +51,29 @@ public class Solution {
         }
       
 		if (low < j)
-			quickSort(array, low, j);
+			quickSortReversed(array, low, j);
 		if (i < high)
-			quickSort(array, i, high);
+			quickSortReversed(array, i, high);
 	}
-    
-  /*  
-    Prep stage:
-create map with:
-keys : values from input list,
-values : indexes of values from input list,
-make a copy of sorted input list
-Algo stage:
-Iterate through input list, and compare current value (lets call it cv) against value from sorted list (lets call it scv). If it is diffrent:
-increment number of swaps
-get index of the scv from map - map[scv],
-in input list swap cv with scv,
-update map[cv]=map[scv] (map[scv] does not need to be updated as we are not going to use it any more)
-Then you need to execute it on input list and reversed input list - the smaller return value - is the answer.
-    */
     
     public static int getSwapsNo(int[] arr, int[]arr_sorted, HashMap<Integer, Integer> hash) {
         int count = 0;
+
+        // count the swaps number
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != arr_sorted[i]) {
                 count++;
-          //      System.out.println(arr[i]  + " " + i);
+                // change the value - index within the hashmap
                 int index = hash.get(arr_sorted[i]);
+                hash.put(arr_sorted[i], i);
                 hash.put(arr[i], index);
+                // change the values
                 int temp = arr[i];
                 arr[i] = arr_sorted[i];
                 arr[index] = temp;
             }
         }
-        // System.out.println("------");
+
         return count;
     }
     
@@ -94,6 +83,8 @@ Then you need to execute it on input list and reversed input list - the smaller 
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         int[] array = new int[n];
+        
+        // store each value - index pair into the hashmaps
         HashMap<Integer, Integer> hash = new HashMap<>();
         HashMap<Integer, Integer> hash2 = new HashMap<>();
         for (int i = 0; i < n; i++) {
@@ -101,17 +92,24 @@ Then you need to execute it on input list and reversed input list - the smaller 
             hash.put(array[i], i);
             hash2.put(array[i], i);
         }
+        
+        // create a copy of the original array
         int[] array2 = Arrays.copyOf(array, array.length);
+        
+        // create two other arrays sorted in increasing/decreasing order
         int[] array_copy1 = Arrays.copyOf(array, array.length);
         int[] array_copy2 = Arrays.copyOf(array, array.length);
         quickSort(array_copy1, 0, n - 1);
         quickSortReversed(array_copy2, 0, n - 1);
         
+        // compute the swaps number for both sorted arrays and return the
+        // minimum value
         int swapsSorted = getSwapsNo(array, array_copy1, hash);
         int swapsSortedReversed = getSwapsNo(array2, array_copy2, hash2);
         if (swapsSorted < swapsSortedReversed)
             System.out.println(swapsSorted);
         else
             System.out.println(swapsSortedReversed);
+
     }
 }
