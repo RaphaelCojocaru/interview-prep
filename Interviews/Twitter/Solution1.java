4636116547e0a2954e5664cdebc3f520be429dad
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-08-03 17:04:46 +0300

    Twitter technical test - problem

---
+++ Interviews/Twitter/Solution1.java
@@ -0,0 +1,11 @@
+package Twitter;
+
+public class Solution1 {
+	/*
+		 
+select sum(distinct a.TIV_2012) from INSURANCE a, INSURANCE b
+where (a.TIV_2011 = b.TIV_2011) and (a.PID != b.PID)
+and (((select count(PID) from INSURANCE where LAT = a.LAT) = 1) or ((select count(PID) from INSURANCE where LON = a.LON) = 1));
+	 
+	 */
+}
