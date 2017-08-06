815bdc0a2224957968b2b508b1200b54676bc77b
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-08-06 22:27:34 +0300

    inorder traversal

---
+++ HackerRank/Data Structures/Trees/tree-inorder-traversal/Solution.java
@@ -0,0 +1,19 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+// https://www.hackerrank.com/challenges/tree-inorder-traversal
+void inOrder(Node root) {
+    if (root == null)
+        return;
+    
+    inOrder(root.left);
+    System.out.print(root.data + " ");
+    inOrder(root.right);
+}
