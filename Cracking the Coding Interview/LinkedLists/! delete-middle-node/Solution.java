edcfb6b0ac0acd7db225db0987c33987d1d5c2a7
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-08-07 17:45:03 +0300

    smarter solution, performing only one copy

--- Cracking the Coding Interview/LinkedLists/! delete-middle-node/Solution.java
+++ Cracking the Coding Interview/LinkedLists/! delete-middle-node/Solution.java
@@ -15,7 +15,21 @@ public class Solution {
 		if (prev != null)
 			prev.next = null;
 	}
+	
+	public static void removeMiddle2(Node list) {
+		Node next = null;
 
+		if (list == null || list.next == null)
+			return;
+
+		// get the next node
+		next = list.next;
+		// overwrite current info with the following info
+		list.info = next.info;
+		// remove the following node, as it is a duplicate now
+		list.next = next.next;
+	}
+	
 	public static void printList(Node list) {
 		for (; list != null; list = list.next)
 			System.out.print(list.info + " ");
