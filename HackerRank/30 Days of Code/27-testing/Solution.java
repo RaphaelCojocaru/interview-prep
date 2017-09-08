83b562e951238ec734d73f4a84271585318bcc59
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-09-08 18:36:31 +0300

    day 27

---
+++ HackerRank/30 Days of Code/27-testing/Solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// https://www.hackerrank.com/challenges/30-testing
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        
+        System.out.println("4 3");
+        System.out.println("-1 -3 4 2");
+
+        System.out.println("5 2");
+        System.out.println("0 -1 2 1 4");
+        
+        System.out.println("6 5");
+        System.out.println("0 -1 -2 -1 4 5");
+        
+        System.out.println("7 2");
+        System.out.println("0 0 -1 2 1 4 5");
+        
+        System.out.println("7 7");
+        System.out.println("0 0 -1 2 1 4 5");
+    }
+}
