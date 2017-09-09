260d9f1df15efb7a46624a06b54395a280f50008
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-09-09 19:30:13 +0300

    java regex for an ip validation

---
+++ HackerRank/Java/Strings/! java-regex-ip/Solution.java
@@ -0,0 +1,7 @@
+//Write your code here
+// https://www.hackerrank.com/challenges/java-regex
+class MyRegex {
+    String pattern = "([0-1]?[0-9]?[0-9]?|2([0-4][0-9]|5[0-5]))\\.([0-1]?[0-9]?[0-9]?|2([0-4][0-9]|5[0-5]))\\.([0-1]?[0-9]?[0-9]?|2([0-4][0-9]|5[0-5]))\\.([0-1]?[0-9]?[0-9]?|2([0-4][0-9]|5[0-5]))";
+    public MyRegex() {
+    }
+}
