63c845f1bd977cf91a89960ecb0985952ad53ce1
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-09-09 19:36:38 +0300

    valid username checker

---
+++ HackerRank/Java/Strings/! valid-username-checker/Solution.java
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/valid-username-checker
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z][\\w]{7, 29}$";
+}
