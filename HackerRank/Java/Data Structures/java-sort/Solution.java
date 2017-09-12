c71d8823d0b206644fde3b160ff32acaf9a2248d
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-09-12 11:17:14 +0300

    java-sort

---
+++ HackerRank/Java/Data Structures/java-sort/Solution.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+// https://www.hackerrank.com/challenges/java-sort
+class Student implements Comparable<Student> {
+    private int id;
+    private String fname;
+    private double cgpa;
+    
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getFname() {
+        return fname;
+    }
+    
+    public double getCgpa() {
+        return cgpa;
+    }
+    
+    public int compareTo(Student stud) {
+        if (this.cgpa < stud.cgpa)
+            return 1;
+        if (this.cgpa > stud.cgpa)
+            return -1;
+        
+        if (this.fname.equals(stud.fname) == false)
+            return this.fname.compareTo(stud.fname);
+        
+        return this.id - stud.id;
+    }
+}
+
+//Complete the code
+public class Solution {
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      
+      List<Student> studentList = new ArrayList<Student>();
+      while (testCases > 0) {
+         int id = in.nextInt();
+         String fname = in.next();
+         double cgpa = in.nextDouble();
+         
+         Student st = new Student(id, fname, cgpa);
+         studentList.add(st);
+         
+         testCases--;
+      }
+      
+      Collections.sort(studentList);
+       
+      for(Student st: studentList) {
+         System.out.println(st.getFname());
+      }
+   }
+}
