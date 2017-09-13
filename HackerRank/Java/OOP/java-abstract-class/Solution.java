12eca9afcbe430ba758a4fa485bb7da193e11fbb
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-09-13 17:10:55 +0300

    java-abstract-class

---
+++ HackerRank/Java/OOP/java-abstract-class/Solution.java
@@ -0,0 +1,8 @@
+// https://www.hackerrank.com/challenges/java-abstract-class
+// Write MyBook class here
+class MyBook extends Book {
+    @Override
+    void setTitle(String s) {
+        this.title = s;
+    }
+}
