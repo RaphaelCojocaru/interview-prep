cd1b092f6a8e32604dfbff4b3a36f63ad1895f79
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2017-09-13 17:00:43 +0300

    java-inheritance-2

---
+++ HackerRank/Java/OOP/java-inheritance-2/Solution.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/java-inheritance-2
+// Write your code here
+class Arithmetic {
+    public int add(int x, int y ) {
+        return (x+y);
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
