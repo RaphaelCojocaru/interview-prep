93e31f9df2616d3db9e9294168efdb0d86f2f55f
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-04-28 10:59:47 +0300

    java-int-to-string

---
+++ HackerRank/Java/Introduction/java-int-to-string/Solution2.java
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/java-int-to-string/problem
+
+Scanner in = new Scanner(System.in);
+int n = in .nextInt();
+in.close();
+//String s=???; Complete this line below
+String s = String.valueOf(n);
