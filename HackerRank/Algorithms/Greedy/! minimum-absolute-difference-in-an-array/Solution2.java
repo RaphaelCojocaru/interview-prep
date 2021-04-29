913a2870b7d93b0210723b34ea8f8835274f68ec
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-04-29 20:14:05 +0300

    ! minimum-absolute-difference-in-an-array

---
+++ HackerRank/Algorithms/Greedy/! minimum-absolute-difference-in-an-array/Solution2.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int minimumAbsoluteDifference(List<Integer> list) {
+        int minDiff = Integer.MAX_VALUE;
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+
+        insertionSort(arr);
+
+        for (int i = 1; i < arr.length; i++) {
+            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        return minDiff;
+    }
+
+    public static void insertionSort(int[] arr) {
+        int j, value;
+
+        for (int i = 1; i < arr.length; i++) {
+            value = arr[i];
+            j = i;
+
+            while (j > 0 && arr[j - 1] > value) {
+                arr[j] = arr[j - 1];
+
+                j--;
+            }
+
+            arr[j] = value;
+        }
+    }
+}
+
+// https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
