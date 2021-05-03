17a8b5bb84ed29df80aab28d861473398276ef31
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-05-03 20:29:33 +0300

    sparse-arrays

---
+++ HackerRank/Data Structures/Arrays/sparse-arrays/Solution2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+// https://www.hackerrank.com/challenges/sparse-arrays/problem
+class Solution2 {
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        List<Integer> results = new ArrayList();
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String string : strings) {
+            if (map.containsKey(string)) {
+                map.put(string, map.get(string) + 1);
+            } else {
+                map.put(string, 1);
+            }
+        }
+
+        for (String query : queries) {
+            if (map.containsKey(query)) {
+                results.add(map.get(query));
+            } else {
+                results.add(0);
+            }
+        }
+
+        return results;
+    }
+}
