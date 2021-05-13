51d02adc0b350c97ce1559ff2548dd9256c35901
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-05-13 21:53:38 +0300

    the-love-letter-mystery

---
+++ HackerRank/Algorithms/Strings/the-love-letter-mystery/Solution2.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'theLoveLetterMystery' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int theLoveLetterMystery(String s) {
+        int noOfOperations = 0, i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            noOfOperations += Math.abs(s.charAt(i) - s.charAt(j));
+
+            i++;
+            j--;
+        }
+
+        return noOfOperations;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/the-love-letter-mystery/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = bufferedReader.readLine();
+
+            int result = Result.theLoveLetterMystery(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
