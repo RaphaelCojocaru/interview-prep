7c10fc691e0d9ba69b03affd677a4c5b25b0b8e4
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-05-16 21:16:38 +0300

    tutorial-intro

---
+++ HackerRank/Algorithms/Sorting/tutorial-intro/Solution2.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'introTutorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER V
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int introTutorial(int value, List<Integer> list) {
+        int[] arr = new int[list.size()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+
+        return binarySearch(arr, value, 0, arr.length - 1);
+    }
+
+    public static int binarySearch(int[] arr, int value, int low, int high) {
+        int middle = low + (high - low) / 2;
+
+        if (low > high) {
+            return -1;
+        }
+
+        if (value == arr[middle]) {
+            return middle;
+        }
+
+        if (value < arr[middle]) {
+            return binarySearch(arr, value, low, high - 1);
+        }
+
+        return binarySearch(arr, value, middle + 1, high);
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/tutorial-intro/problem
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int V = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.introTutorial(V, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
