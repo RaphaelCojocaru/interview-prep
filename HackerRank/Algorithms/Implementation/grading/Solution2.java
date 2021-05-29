de9458af7c1b97ab1c1f37b0a56865bedc8db7ba
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-05-29 20:01:25 +0300

    grading

---
+++ HackerRank/Algorithms/Implementation/grading/Solution2.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+// https://www.hackerrank.com/challenges/grading/problem
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        ArrayList<Integer> roundedResults = new ArrayList<Integer>();
+
+        for (int grade : grades) {
+            if (grade > 37 && grade % 5 >= 3) {
+                roundedResults.add(grade + (5 - grade % 5));
+            } else {
+                roundedResults.add(grade);
+            }
+        }
+
+        return roundedResults;
+    }
+}
+
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = new ArrayList<>();
+
+        for (int i = 0; i < gradesCount; i++) {
+            int gradesItem = Integer.parseInt(bufferedReader.readLine().trim());
+            grades.add(gradesItem);
+        }
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
