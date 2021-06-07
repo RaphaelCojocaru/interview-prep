1b1044577c3754e5ca6e0475349497447a8c0006
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-07 20:43:02 +0300

    jumping-on-the-clouds

---
+++ HackerRank/Algorithms/Implementation/jumping-on-the-clouds/Solution2.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        int start = 0, end = c.size() - 1, jumps = 0;
+
+        while (start < end) {
+            if (start + 2 < c.size() && c.get(start + 2) == 0) {
+                start += 2;
+            } else {
+                start += 1;
+            }
+
+            jumps += 1;
+        }
+
+        return jumps;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] cTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> c = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cTemp[i]);
+            c.add(cItem);
+        }
+
+        int result = Result.jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
