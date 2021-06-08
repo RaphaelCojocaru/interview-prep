13808484d896e6ccb1b67e54afc0604c773fc490
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-08 20:45:45 +0300

    equality-in-a-array

---
+++ HackerRank/Algorithms/Implementation/equality-in-a-array/Solution2.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static int equalizeArray(List<Integer> arr) {
+        int[] occurrences = new int[101];
+        int maxOccurrence = -1;
+
+        for (int value : arr) {
+            occurrences[value]++;
+
+            maxOccurrence = Math.max(maxOccurrence, occurrences[value]);
+        }
+
+        return arr.size() - maxOccurrence;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/equality-in-a-array/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
