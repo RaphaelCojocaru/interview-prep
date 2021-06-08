502a2d052434d8e85d23832ecd4e6810f99c9165
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-08 20:44:33 +0300

    cut-the-sticks

---
+++ HackerRank/Algorithms/Implementation/cut-the-sticks/Solution2.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'cutTheSticks' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> cutTheSticks(List<Integer> arr) {
+        List<Integer> sticksLeft = new ArrayList<>();
+
+        int min = Integer.MAX_VALUE;
+
+        Collections.sort(arr);
+
+        while (arr.size() > 0) {
+            sticksLeft.add(arr.size());
+
+            min = arr.get(0);
+
+            for (int i = 0; i < arr.size(); i++) {
+                arr.set(i, arr.get(i) - min);
+
+                if (arr.get(i) <= 0) {
+                    arr.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        return sticksLeft;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/cut-the-sticks/problem
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> result = Result.cutTheSticks(arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
