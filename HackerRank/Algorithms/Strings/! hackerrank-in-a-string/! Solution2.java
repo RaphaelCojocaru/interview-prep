53a6c85c2eb098709d93afbfb618e1377c03c239
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-13 21:21:42 +0300

    ! hackerrank-in-a-string

---
+++ HackerRank/Algorithms/Strings/! hackerrank-in-a-string/! Solution2.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String hackerrankInString(String s) {
+        String match = "hackerrank";
+
+        int index = 0;
+
+        for (int i = 0; i < s.length() && index < match.length(); i++) {
+            if (s.charAt(i) == match.charAt(index)) {
+                index++;
+            }
+        }
+
+        if (index == match.length()) {
+            return "YES";
+        }
+
+        return "NO";
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = bufferedReader.readLine();
+
+            String result = Result.hackerrankInString(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
