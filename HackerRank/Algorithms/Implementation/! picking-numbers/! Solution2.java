3a4c5788c7c511627670110bd3400e0365ffbb7c
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-15 21:26:32 +0300

    ! picking-numbers

---
+++ HackerRank/Algorithms/Implementation/! picking-numbers/! Solution2.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> arr) {
+        int maxLength = 0;
+        int[] occurrences = new int[100];
+
+        for (int value : arr) {
+            occurrences[value]++;
+        }
+
+        for (int i = 1; i < 100; i++) {
+            maxLength = Math.max(maxLength, occurrences[i] + occurrences[i - 1]);
+        }
+
+        return maxLength;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/picking-numbers/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
