5b69d37c1f3b3989a46d3af41f199d44b5d70f3d
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-18 21:14:14 +0300

    lisa-workbook

---
+++ HackerRank/Algorithms/Implementation/lisa-workbook/Solution2.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'workbook' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY arr
+     */
+    public static int workbook(int n, int k, List<Integer> arr) {
+        int noOfSpecials = 0, page = 1;
+
+        for (int value : arr) {
+            for (int prob = 1; prob <= value; prob++) {
+                if (prob == page) {
+                    noOfSpecials++;
+                }
+
+                if (prob % k == 0) {
+                    page++;
+                }
+            }
+
+            if (value % k != 0) {
+                page++;
+            }
+        }
+
+        return noOfSpecials;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/lisa-workbook/problem
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.workbook(n, k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
