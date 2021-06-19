91b919e96324a67b52195a636ec981f32ac412e5
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-19 21:12:34 +0300

    ! minimum-distances

---
+++ HackerRank/Algorithms/Implementation/! minimum-distances/Solution2.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+        int minDistance = Integer.MAX_VALUE;
+        HashMap<Integer, Integer> lastIndex = new HashMap<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            if (lastIndex.containsKey(a.get(i))) {
+                minDistance = Math.min(minDistance, i - lastIndex.get(a.get(i)));
+            }
+
+            lastIndex.put(a.get(i), i);
+        }
+
+        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/minimum-distances/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
