401279a83fbd62449e59c27ec38fc429f2299efb
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-19 21:12:44 +0300

    beautiful-triplets

---
+++ HackerRank/Algorithms/Implementation/beautiful-triplets/Solution2.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulTriplets' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int beautifulTriplets(int d, List<Integer> arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int triplets = 0;
+
+        for (int value : arr) {
+            if (map.containsKey(value)) {
+                map.put(value, map.get(value) + 1);
+            } else {
+                map.put(value, 1);
+            }
+        }
+
+
+        for (int value : arr) {
+            if (map.containsKey(value) && map.containsKey(value + d) && map.containsKey(value + 2 * d)) {
+                triplets += map.get(value) * map.get(value + d) * map.get(value + 2 * d);
+
+                map.remove(value);
+            }
+        }
+
+        return triplets;
+    }
+
+}
+
+// https://www.hackerrank.com/challenges/beautiful-triplets/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.beautifulTriplets(d, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
