792892b8efb857f7f43d4972d80bc1046464ee1a
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-06-27 08:43:54 +0300

    ! two-strings

---
+++ HackerRank/Algorithms/Strings/! two-strings/Solution3.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// https://www.hackerrank.com/challenges/two-strings
+public class Solution3 {
+
+    static String twoStrings(String s1, String s2){
+        // Complete this function
+        HashSet<Character> set = new HashSet<Character>();
+        
+        for (char ch : s1.toCharArray())
+            set.add(ch);
+        
+        for (char ch : s2.toCharArray())
+            if (set.contains(ch))
+                return "YES";
+        
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s1 = in.next();
+            String s2 = in.next();
+            String result = twoStrings(s1, s2);
+            System.out.println(result);
+        }
+    }
+}
