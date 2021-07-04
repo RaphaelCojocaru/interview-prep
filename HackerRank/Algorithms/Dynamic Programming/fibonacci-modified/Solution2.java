1daccd5d4af39e17dcb1f49870e9900aae2264c3
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-07-04 09:01:36 +0300

    fibonacci-modified

---
+++ HackerRank/Algorithms/Dynamic Programming/fibonacci-modified/Solution2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'fibonacciModified' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER t1
+     *  2. INTEGER t2
+     *  3. INTEGER n
+     */
+
+    public static BigInteger fibonacciModified(int t1, int t2, int n) {
+        return fibo(BigInteger.valueOf(t1), BigInteger.valueOf(t2), n - 2);
+    }
+
+    public static BigInteger fibo(BigInteger big1, BigInteger big2, int n) {
+        if (n == 0) {
+            return big2;
+        }
+
+        return fibo (big2, big1.add(big2.multiply(big2)), n - 1);
+    }
+}
+
+// https://www.hackerrank.com/challenges/fibonacci-modified/problem
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int t1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int t2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n = Integer.parseInt(firstMultipleInput[2]);
+
+        BigInteger result = Result.fibonacciModified(t1, t2, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
