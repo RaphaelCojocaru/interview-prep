bca3be5b4aac7b2db588f59f9b687cf6f29599f5
RaphaelCojocaru <raphael.cojocaru@yahoo.com>
2021-07-07 06:11:07 +0300

    height-of-a-binary-tree

---
+++ HackerRank/Data Structures/Trees/height-of-a-binary-tree/Solution2.java
@@ -0,0 +1,14 @@
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+    //https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+	public static int height(Node root) {
+      	if (root == null) {
+            return -1;
+        }
+
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
